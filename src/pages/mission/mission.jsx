56c1eb9c221c3bc07d353c6cf91fd6059818ba2c
itachiszep <itachiszep@int.pl>
2025-11-28 17:03:47 +0100

    Update mission.jsx

--- src/pages/mission/mission.jsx
+++ src/pages/mission/mission.jsx
@@ -31,7 +31,6 @@ export default function Home() {
   return (
     <div className="bg-black text-white box-border m-0 p-0">
       <div className="w-full bg-black flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 px-3 sm:px-4 md:px-6 lg:px-8 py-6 sm:py-8 md:py-10 lg:py-12 pt-20 md:pt-24 text-center">
-        {/* Obraz */}
         {/* Iframy jedno pod drugim */}
         <div className="w-full flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-4xl">
           {videos.map((videoId, index) => (
@@ -41,7 +40,7 @@ export default function Home() {
             >
               <iframe 
                 className="w-full h-full"
-                src={`https://www.youtube.com/embed/${videoId}?controls=1&modestbranding=0`}
+                src={`https://www.youtube.com/embed/${videoId}?controls=1&modestbranding=0&iv_load_policy=3`}
                 title={`YouTube video ${index + 1}`}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
                 allowFullScreen
