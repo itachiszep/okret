6f594a984262e1f334fdb280fca67fccf2b09ac8
itachiszep <itachiszep@int.pl>
2025-12-07 19:16:43 +0100

    update
    update

--- src/pages/mission/mission.jsx
+++ src/pages/mission/mission.jsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+
 export default function Home() {
-  
   const videos = [
     "wnHW6o8WMas",
     "G8RnMeuJULw",
@@ -19,14 +19,14 @@ export default function Home() {
     "GLheiLGZ1k8",
     "D_Vg4uyYwEk",
     "fvFuGq-1Z8M",
-    "7u9JDhwJXTc", 
-    "ZtLlfdyDySY",     
-    "pLnGByBsFqE",  
-    "BsblgcBB17A",    
-    "y4-in1XbVEc",     
-    "0X-bcrwmEBc",     
-    "h3Nqbx7mbAo",     
-    "1eiFc8g1VXI",    
+    "7u9JDhwJXTc",
+    "ZtLlfdyDySY",
+    "pLnGByBsFqE",
+    "BsblgcBB17A",
+    "y4-in1XbVEc",
+    "0X-bcrwmEBc",
+    "h3Nqbx7mbAo",
+    "1eiFc8g1VXI",
     "H0NCRHpKuO0",
     "wtHrDSR6100",
     "H4BXkyj9dJE",
@@ -35,21 +35,17 @@ export default function Home() {
     "JzVt7CznI9k",
     "HWmqLXP1-o0"
   ];
+
   return (
     <div className="bg-black text-white box-border m-0 p-0">
       <div className="w-full bg-black flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 px-3 sm:px-4 md:px-6 lg:px-8 py-6 sm:py-8 md:py-10 lg:py-12 pt-20 md:pt-24 text-center">
-        {/* Iframy jedno pod drugim */}
         <div className="w-full flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-4xl">
           {videos.map((videoId, index) => (
-            <div 
+            <div
               key={index}
               className="w-full aspect-video rounded overflow-hidden shadow-lg hover:shadow-xl transition-shadow"
             >
-              <video controls>
-                <source src="video.webm" type="video/webm">
-                </source>
-              </video>
-              <iframe 
+              <iframe
                 className="w-full h-full"
                 src={`https://www.youtube.com/embed/${videoId}?controls=1&modestbranding=1&fs=1&iv_load_policy=3&rel=0&showinfo=0&cc_load_policy=0`}
                 title={`YouTube video ${index + 1}`}
